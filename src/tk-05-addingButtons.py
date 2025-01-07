878897dc7145d6d377be161a3c89beb364a01a88
Amulya Jain <jainamulyawin@gmail.com>
2025-01-07 22:58:40 +0530

    modified description

--- src/tk-05-addingButtons.py
+++ src/tk-05-addingButtons.py
@@ -13,6 +13,12 @@ textbox = tk.Text(root, height=3, font=('Arial', 16))
 textbox.pack(padx=10, pady=10)
 
 button = tk.Button(root, text="Click Me!", font=('Arial', 18))
-button.pack()
+button.pack(padx=10, pady=10)
+
+'''
+Similarly we have tk.CheckBox(), 
+tk.frame() - which allows to group multiple buttons
+tk.ListBox(), we have menus,..
+'''
 
 root.mainloop()
